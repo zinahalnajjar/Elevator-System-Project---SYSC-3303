e7d6abd18e73ccd00a74cae3e66ad2ac0f773f6f
MacK Wallace <mackwallacebusiness@gmail.com>
2022-02-01 23:55:54 -0500

    added a few things to the scheduler, will make my own branch

--- src/SchedulerSubsystem.java
+++ src/SchedulerSubsystem.java
@@ -1,13 +1,12 @@
 /**
- * works as a channel to communicate data between the floors and the elevator 
- * receives an input data from the floor subsystem 
- * receives data from the elevator subsystem 
+ * works as a channel to communicate data between the floors and the elevator
+ * receives an input data from the floor subsystem
+ * receives data from the elevator subsystem
  * sends data back to the elevator subsystem
- * sends data back to the floor subsystem 
- * @author MacKenzie Wallace, Zinah Al-Najjar
- * @version Fab 5, 2021
+ * sends data back to the floor subsystem
+ * @Author MacKenzie Wallace
+ * @version Feb 5, 2021
  */
-
 public class SchedulerSubsystem implements Runnable{
 
 	@Override
