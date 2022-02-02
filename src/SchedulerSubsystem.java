ef59c58f9dea263e0549f9085cc1c0bc5958f280
MacK Wallace <mackwallacebusiness@gmail.com>
2022-02-01 23:09:53 -0500

    testing commit from eclipse

--- src/SchedulerSubsystem.java
+++ src/SchedulerSubsystem.java
@@ -1,14 +1,18 @@
-public class SchedulerSubsystem implements Runnable{
-/*
+/**
  * works as a channel to communicate data between the floors and the elevator 
  * receives an input data from the floor subsystem 
  * receives data from the elevator subsystem 
  * sends data back to the elevator subsystem
  * sends data back to the floor subsystem 
- * 
+ * @author MacKenzie Wallace, Zinah Al-Najjar
+ * @version Fab 5, 2021
  */
+
+public class SchedulerSubsystem implements Runnable{
+
 	@Override
 	public void run() {
+		//testing 
 		// TODO Auto-generated method stub
 		
 	}
