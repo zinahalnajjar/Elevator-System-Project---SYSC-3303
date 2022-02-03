289eef7e1495aa8ad9265e0b3ddd963ba7205553
vilmoose <vilmosf11@gmail.com>
2022-02-03 18:32:48 -0500

    test push

--- src/ElevatorSubsystem.java
+++ src/ElevatorSubsystem.java
@@ -1,8 +1,33 @@
 
 public class ElevatorSubsystem implements Runnable {
+	
+	private int elevatorID;
+	private int currentFloor;
+	private int destinationFloor;
+	private boolean lampOn;
+	
+	
+	/**
+	 * Constructor
+	 */
+	public ElevatorSubsystem() {
+		
+	}
+	
+	public void handleRequest() {
+		
+	}
+	
+	public void sendData() {
+		
+	}
+	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		// check for requests from scheduler, then handle requests
+		while(true) {
+			
+		}
 		
 	}
 
