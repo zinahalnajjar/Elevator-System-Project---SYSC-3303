6f46ceb0cc4670612443ba799c85c3ebf6e4c599
MacK Wallace <mackwallacebusiness@gmail.com>
2022-02-04 18:55:07 -0500

    finished up the code *DOES NOT WORK

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,3 +1,16 @@
 public class Simulation {
 
+	/**
+	 * Main thread
+	 */
+	public static void main(String[] args) {
+		SchedulerSubsystem scheduler = new SchedulerSubsystem();
+		Thread floor = new Thread(new FloorSubsystem(scheduler), "Floor");
+		Thread sched= new Thread(new SchedulerSubsystem(), "Scheduler");
+		Thread elevator = new Thread(new ElevatorSubsystem(scheduler, 1), "Floor");
+		
+		elevator.start();
+		sched.start();
+		floor.start();
+	}
 }
