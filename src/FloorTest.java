1790351a0360cbb43e80ed51ba6731815641f89b
MacK Wallace <mackwallacebusiness@gmail.com>
2022-03-31 12:58:39 -0400

    added a test case for the error handling

--- src/FloorTest.java
+++ src/FloorTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.UnknownHostException;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
@@ -15,21 +16,23 @@ public class FloorTest {
 	
 	/**
 	 * This method tests the getInfo(0 method to see if it properly reads the input file
+	 * @throws UnknownHostException 
 	 */
 	@Test
-	public void testRead() {
-		FloorRequestData floorReqData = new FloorRequestData();
-		FloorRequest elevatorReq = new FloorRequest();
-		SchedulerSubsystem sc = new SchedulerSubsystem(floorReqData, elevatorReq);
-		FloorSubsystem floor = new FloorSubsystem(sc, floorReqData);
+	public void testRead() throws UnknownHostException {
+		//FloorRequestData floorReqData = new FloorRequestData();
+		//FloorRequest elevatorReq = new FloorRequest();
+		//SchedulerSubsystem sc = new SchedulerSubsystem(floorReqData, elevatorReq);
+		MainFloorSys floor = new MainFloorSys();
 		LocalTime localTime = LocalTime.of(2, 11,16,573 * 1000000);
-		//FloorMovementData testCase = new FloorMovementData(localTime, 3, 6, true);
+		FloorMovementData testCase = new FloorMovementData(localTime, 3, 6, true, 2);
 		ArrayList<FloorMovementData> floorList = floor.getInfo();
 		
-		//assertEquals(floorList.get(1).getOriginFloor(), testCase.getOriginFloor());
-		//assertEquals(floorList.get(1).getDestinationFloor(), testCase.getDestinationFloor());
-		//assertEquals(floorList.get(1).getLocalTime(), testCase.getLocalTime());
-		//assertFalse(floorList.get(1).isGoingUp());
+		assertEquals(floorList.get(1).getOriginFloor(), testCase.getOriginFloor());
+		assertEquals(floorList.get(1).getDestinationFloor(), testCase.getDestinationFloor());
+		assertEquals(floorList.get(1).getLocalTime(), testCase.getLocalTime());
+		assertFalse(floorList.get(1).isGoingUp());
+		assertEquals(floorList.get(1).getError(), testCase.getError());
 
 	}
 }
