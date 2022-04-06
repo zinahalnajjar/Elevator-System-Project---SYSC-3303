a472f25463ba05abafe751a4ccbaa9f8df533b90
MacK Wallace <mackwallacebusiness@gmail.com>
2022-04-06 18:16:59 -0400

    added a couple more tests

--- src/ElevatorTest.java
+++ src/ElevatorTest.java
@@ -1,8 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-
+import java.net.SocketException;
 
 import org.junit.jupiter.api.Test;
+
 
 /**
  * Test Class for Elevator
@@ -12,17 +13,40 @@ import org.junit.jupiter.api.Test;
 public class ElevatorTest {
 
 	/**
-	 * tests that motor can turn on and therefore indicates that the elevator can move
+	 * tests that motor can turn off and on and therefore indicates that the elevator can move
 	 */
 	@Test
 	public void testIsMoving(){
 		FloorRequest floorReq = new FloorRequest();
 		Elevator elevator = new Elevator(1,floorReq);
-		Thread elThread = new Thread(elevator);
-		elThread.start();
-		assertFalse(elevator.isMotorOperating());
+		assertFalse(elevator.isMotorOperating()); // check if motor is off
 		elevator.motorsOn();
-		assertTrue(elevator.isMotorOperating());
+		assertTrue(elevator.isMotorOperating()); // check if motor is on
+		
+	}
+	
+	/**
+	 * tests the initial current floor the elevator is on
+	 */
+	@Test
+	public void testCurrentFloor() {
+		FloorRequest floorReq = new FloorRequest();
+		Elevator elevator = new Elevator(2,floorReq);
+		assertEquals(0,elevator.getCurrentFloor());
+	}
+	
+	/**
+	 * test the initial state of the elevator
+	 */
+	@Test
+	public void testStillState() {
+		FloorRequest floorReq = new FloorRequest();
+		Elevator elevator = new Elevator(2,floorReq);
+		assertEquals("STILL", elevator.getCurrentState());
+	}
+	
+	@Test
+	public void testLoading() {
 		
 	}
 		
